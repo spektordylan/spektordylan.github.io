2bb945f934fed128fba4b0ce01e48e78c4468f33
Dylan Spektor <dylanspek07@gmail.com>
2022-12-30 18:52:10 -0800

    General changes to content, animations, and links.

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,12 +18,29 @@ module.exports = {
             '50%': {
                 transform: 'rotate(6deg)'
             },
-        }
+        },
+        wave: {
+            '0%, 60%, 100%' : { 
+                transform: 'rotate(0.0deg)' 
+            },
+            '10%, 30%' : { 
+                transform: 'rotate(28.0deg)' 
+            },
+            '20%' : { 
+                transform: 'rotate(-16.0deg)' 
+            },
+            '40%' : { 
+                transform: 'rotate(-8.0deg)' 
+            },
+            '50%' : { 
+                transform: 'rotate(20.0deg)' 
+          },
+        },
+      }
     },
     animation: {
         wiggle: 'wiggle 2s ease-in-out infinite',
+        wave: 'wave 2.5s infinite',
     }
     },
-  },
-  plugins: [],
-};
+  };
